43d83532ad805c1db9243f2e7c1eba8bd87b640c
Kronapsys <rsxvii@gmail.com>
2021-02-19 20:41:33 +0100

    added default new date on rental date from order schema

--- models/order.js
+++ models/order.js
@@ -8,7 +8,8 @@ let orderSchema = mongoose.Schema({
     },
     rentalDate:{
         type: Date,
-        required: true
+        required: true,
+        default: new Date
     },
     returnDate:{
         type: Date,
