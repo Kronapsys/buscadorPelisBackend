cd6ae8b6d58ade409aa84abc13b38781ad21bee1
Kronapsys <rsxvii@gmail.com>
2021-02-19 20:30:07 +0100

    order model changed to order schema

--- models/order.js
+++ models/order.js
@@ -1,8 +1,20 @@
-class Order{
-    
-    constructor(){
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
 
+let orderSchema = mongoose.Schema({
+    id:{
+        type: ObjectId,
+        required: true
+    },
+    rentalDate:{
+        type: Date,
+        required: true
+    },
+    returnDate:{
+        type: Date,
+        required: true
     }
-}
+});
 
-module.exports = Order;
+
+module.exports = mongoose.model('order', orderSchema );
