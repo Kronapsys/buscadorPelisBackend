0bae14b96a16dac630a599afdcb5346b7c921fcd
Kronapsys <rsxvii@gmail.com>
2021-02-22 20:20:44 +0100

    app.js modified

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const router = require('./router');
+const db = require('./db');
 
-// Server up
-app.listen(port, () => console.log(`Listening at ${port}`));
+const app = express()
+const port = 3000
+
+app.use(express.json());
+app.use(router);
+
+
+
+
+db.then(() => {
+    app.listen(port, () => {
+        console.log(`Server app listening at http://localhost:${port}`)
+    })
+}).catch(console.log)
