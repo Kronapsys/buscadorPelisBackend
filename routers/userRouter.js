4a9ea66c069a6111c15bfb2d6b1360f570e648cc
Kronapsys <rsxvii@gmail.com>
2021-02-22 20:16:58 +0100

    userRouter created

---
+++ routers/userRouter.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const userController = require('../controllers/user');
+
+router.get('/',async (req, res) => {   
+    try{
+        res.json(await userController.indexAll())
+    } catch (error) {
+        return res.sendStatus(500).json({
+            message: 'Server Error'
+        });
+    }
+});
+
+router.post('/',async (req, res) => {
+    try{
+        const id = await userController.store(req.body);
+        const status = 'success';
+        res.json({status,id});
+    } catch( error ){
+        return res.sendStatus(500).json({
+            message: 'Server Error'
+        });
+    }
+})
+
+router.put('/:id',async (req,res) => {
+    try{
+        const id = req.params.id;
+        res.json(await userController.update(id,req.body));
+    } catch( error ){
+        return res.sendStatus(500).json({
+            message: 'Server Error'
+        });
+    }
+});
+
+router.delete('/:id',async (req, res) => {
+   try{
+        const id = req.params.id;
+        const status = 'deleted'
+        await userController.destroy(id);
+        res.json({status,id});
+   } catch( error ) {
+    return res.sendStatus(500).json({
+        message: 'Server Error'
+    });   
+   }
+
+});
+
+
+
+module.exports = router;
