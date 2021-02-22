e565dba5b36fa567d74665dbc717df9958345f2f
Kronapsys <rsxvii@gmail.com>
2021-02-22 20:31:51 +0100

    router.js modified

--- router.js
+++ router.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 
+const filmRouter = require('./routers/filmRouter');
 const orderRouter = require('./routers/orderRouter');
 const userRouter = require('./routers/userRouter');
 
 
-router.use('/order',orderRouter);
-router.use('/user',userRouter);
+router.use('/film', filmRouter);
+router.use('/order', orderRouter);
+router.use('/user', userRouter);
 
 
 module.exports = router;
